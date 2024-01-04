7926a179cbd0e145c58d11d21d6b72ddbe162bf9
makecolour <quyennguyen083004@gmail.com>
2024-01-04 22:49:48 +0700

    asd

--- content/process_library.js
+++ content/process_library.js
@@ -1,7 +1,7 @@
 const main = async () => {
-    const rollnum = await getFromStorage('STUDENT_ROLL', '');
-    const password = await getFromStorage('STUDENT_PASS', '');
-    document.getElementById("Username").setAttribute("value", rollnum);
+    const roll = await getFromStorage('STUDENT_ROLL', '');
+	const password = await getFromStorage('STUDENT_PASS', '');
+    document.getElementById("Username").setAttribute("value", roll);
     document.getElementById("Password").setAttribute("value", password);
 }
 main();
--- popup/popup.js
+++ popup/popup.js
@@ -73,4 +73,4 @@ function myFunction() {
 	}
 };
 
-
+v
