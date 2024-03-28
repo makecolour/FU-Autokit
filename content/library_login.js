946fd6d424f2c31a72e57a81e1af16a99aaadbf7
makecolour <quyennguyen083004@gmail.com>
2024-03-29 03:36:36 +0700

    Update library_login.js

--- content/library_login.js
+++ content/library_login.js
@@ -2,20 +2,14 @@ const main = async () => {
     const enabled = await getFromStorage('LBR_1', '');
     if(enabled==true)
     {
-var check = false;
-
 var divs = document.querySelectorAll('.active');
 
 for (i = 0; i < divs.length; ++i) {
     if (divs[i].innerHTML == "Login") {
-        check = true;
+        divs[i].click();
         break;
     }
 };
 
-if (check == true) {
-    window.location.replace("https://library.fpt.edu.vn/Auth/Login");
-}
-
-    }}
+}}
     main()
