3db647837711987af47d2cab77899e73be1b41e1
makecolour <quyennguyen083004@gmail.com>
2024-03-29 15:52:03 +0700

    f

--- content/gsi.js
+++ content/gsi.js
@@ -5,7 +5,8 @@ const main = async () => {
     const email = await getFromStorage('STUDENT_EMAIL', '');
     const edn1_ = await getFromStorage('EDN_1', '');
     const flm1_ = await getFromStorage('FLM_1', '');
-    if((url.includes("flm.fpt.edu.vn")&&flm1_==true)||(url.includes("fu-edunext.fpt.edu.vn")&&edn1_==true))
+    const fap1_ = await getFromStorage('FAP_1', '');
+    if((url.includes("flm.fpt.edu.vn")&&flm1_==true)||(url.includes("fu-edunext.fpt.edu.vn")&&edn1_==true)||(url.includes("googleauthensite01.fpt.edu.vn")&&fap1_==true))
     {
         setTimeout(() =>{for (var i = 0; i < emailEl.length; i++) {
             if (emailEl[i].innerHTML == email.toLowerCase()) {
