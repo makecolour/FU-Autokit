23cba5149cae8d5eb3327fd064c3d49d505d8cac
makecolour <quyennguyen083004@gmail.com>
2024-04-04 21:49:12 +0700

    Update fap_getbit.js

--- content/fap_getbit.js
+++ content/fap_getbit.js
@@ -4,13 +4,29 @@ function searchStringInArray(str, strArray) {
   }
   return -1;
 }
-const bit = document.querySelector("#ctl00_mainContent_lblCurriculum").innerHTML
+
+function getBit() {
+  const bit = document.querySelector("#ctl00_mainContent_lblCurriculum").innerHTML;
+  const bitArr = bit.split("_");
+  if(bitArr[2].includes("A")) {
+    return `${bitArr[0]}_${bitArr[1]}_K${Number.parseInt(bitArr[2]) - 1}D_K${bitArr[2]}`
+  }
+  else{
+    return `${bitArr[0]}_${bitArr[1]}_K${bitArr[2]}`
+  }
+}
+
+
+
 const main = async () => {
   const list = await chrome.storage.local.get('listCurriculum');
+  const bit = getBit();
+  console.log(bit);
   if (searchStringInArray(bit, list.listCurriculum) == -1) {
     list.listCurriculum.push(bit);
     chrome.storage.local.set({ 'listCurriculum': list.listCurriculum });
     console.log("Updated");
   }
 }
-main()
+
+window.onload = main;
