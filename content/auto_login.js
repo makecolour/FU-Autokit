77fa6bb09c57466ef9a745162fef4a17b2f9260a
makecolour <quyennguyen083004@gmail.com>
2024-06-12 01:22:56 +0700

    add

--- content/auto_login.js
+++ content/auto_login.js
@@ -1,4 +1,5 @@
 const url = window.location.href;
+const nextBtn = document.querySelector("#identifierNext > div > button > span");
 const main = async () => {
     const email = await getFromStorage('STUDENT_EMAIL', '');
     const fap1_ = await getFromStorage('FAP_1', '');
@@ -6,14 +7,12 @@ const main = async () => {
     const cms1_ = await getFromStorage('CMS_1', '');
     const crs1_ = await getFromStorage('CRS_1', '');
     const flm1_ = await getFromStorage('FLM_1', '');
+    const ojt1_ = await getFromStorage('OJT_1', '');
     const gen_ = await getFromStorage('K', '');
 
-    if ((url.includes("fap.fpt.edu.vn") && gen_ == false && fap1_ == true) || (url.includes("cmshn.fpt.edu.vn") && gen_ == false && cms1_ == true) || (url.includes("www.coursera.org") && gen_ == false && crs1_ == true) || (url.includes("flm.fpt.edu.vn") && flm1_ == true) || (url.includes("fu-edunext.fpt.edu.vn") && edn1_ == true) || (url.includes("feid.fpt.edu.vn") && gen_ == true) || (url.includes("googleauthensite01.fpt.edu.vn") && fap1_ == true)) {
+    if ((url.includes("ojt.fpt.edu.vn") && gen_ == false && ojt1_ == true) || (url.includes("fap.fpt.edu.vn") && gen_ == false && fap1_ == true) || (url.includes("cmshn.fpt.edu.vn") && gen_ == false && cms1_ == true) || (url.includes("www.coursera.org") && gen_ == false && crs1_ == true) || (url.includes("flm.fpt.edu.vn") && flm1_ == true) || (url.includes("fu-edunext.fpt.edu.vn") && edn1_ == true) || (url.includes("feid.fpt.edu.vn") && gen_ == true) || (url.includes("googleauthensite01.fpt.edu.vn") && fap1_ == true)) {
         document.querySelector("#identifierId").setAttribute('value', email);
-        // setTimeout(() => {
-        //     const nextBtn = document.querySelector(".VfPpkd-Jh9lGc");
-        //     nextBtn.click();
-        // },2000)
+        nextBtn.click();
     }
 }
 window.onload = main();
