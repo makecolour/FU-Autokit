1b6f014c004bb5139a5a90f19b1071ac2224e06e
makecolour <quyennguyen083004@gmail.com>
2024-06-20 08:20:31 +0700

    sorting

--- content/fap_getbit.js
+++ content/fap_getbit.js
@@ -33,6 +33,31 @@ const main = async () => {
     chrome.storage.local.set({ 'listCurriculum': list.listCurriculum });
     console.log("Updated");
   }
+  sorting();
 }
 
 window.onload = main;
+
+function sorting() {
+  const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+  const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
+      v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
+      )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+
+  
+  // This function is triggered when a `<th>` is clicked
+  const onThClick = (th, idx) => {
+    const table = th.closest('table');
+    // Select only rows within the <tbody> to sort
+    const tbody = table.querySelector('tbody');
+    Array.from(tbody.querySelectorAll('tr'))
+      .sort(comparer(idx, this.asc = !this.asc))
+      .forEach(tr => tbody.appendChild(tr));
+  };
+  
+  // Attach click event listeners to all `<th>` elements within <thead> to ensure sorting is applied correctly
+  document.querySelectorAll('.table thead th').forEach(th => th.addEventListener('click', () => onThClick(th, th.cellIndex)));
+
+  // Attach click event listeners to all `<th>` elements
+  document.querySelectorAll('.table th').forEach(th => th.addEventListener('click', () => onThClick(th, th.cellIndex)));
+}
--- content/fap_getcourses.js
+++ content/fap_getcourses.js
@@ -18,5 +18,28 @@ window.onload = () => {
     subjectsName.shift();
     chrome.storage.local.set({ 'subjects': subjects });
     chrome.storage.local.set({ 'subjectsName': subjectsName });
-}
 
+    sorting()
+}
+function sorting() {
+    const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
+    const comparer = (idx, asc) => (a, b) => ((v1, v2) => {
+        // Remove commas for numeric comparison
+        const pattern = /,/g;
+        v1 = v1.replace(pattern, '');
+        v2 = v2.replace(pattern, '');
+        return v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2);
+    })(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
+    
+    // This function is triggered when a `<th>` is clicked
+    const onThClick = (th, idx) => {
+      const table = th.closest('table');
+      const tbody = table.querySelector('tbody');
+      const rows = Array.from(tbody.querySelectorAll('tr:nth-child(n+2)'));
+      rows.sort(comparer(idx, this.asc = !this.asc))
+          .forEach(tr => tbody.appendChild(tr));
+    };
+  
+    // Attach click event listeners to all `<th>` elements in the table header
+    document.querySelectorAll('#ctl00_mainContent_gvSubjects th').forEach(th => th.addEventListener('click', () => onThClick(th, th.cellIndex)));
+}
