05c861e70a3ce8ef9963a630ecedb6a148807041
makecolour <quyennguyen083004@gmail.com>
2024-06-22 19:34:07 +0700

    1

--- content/process_library.js
+++ content/process_library.js
@@ -1,6 +1,14 @@
 const main = async () => {
     const id = await getFromStorage('STUDENT_ROLL', '');
+    if(id==null)
+        {
+            id=="";
+        }
     const pass = await getFromStorage('STUDENT_PASS', '');
+    if(pass==null)
+        {
+            pass="";
+        }
     const enabled = await getFromStorage('LBR_1', '');
     if (enabled == true) {
         document.getElementById("Username").setAttribute("value", id);
